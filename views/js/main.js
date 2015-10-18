23cfd5f9c5092f7981a6452396ef1020b2db2ecc
scott.stubbs650@gmail.com <scstubbs@pacbell.net>
2015-10-17 23:22:43 -0400

    small change to keep under 80

--- views/js/main.js
+++ views/js/main.js
@@ -580,13 +580,14 @@ var resizePizzas = function(size) {
   window.performance.mark("mark_end_resize");
   window.performance.measure("measure_pizza_resize", "mark_start_resize", 
         "mark_end_resize");
-  var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
+ var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
   console.log("Time to resize pizzas: " + timeToResize[0].duration + "ms");
 };
 
 window.performance.mark("mark_start_generating"); // collect timing data
 
-// This for-loop actually creates and appends all of the pizzas when the page loads
+// This for-loop actually creates and appends all of the pizzas when the page 
+    //loads
 for (var i = 2; i < 100; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
